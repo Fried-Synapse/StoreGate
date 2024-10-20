0f5cc589481c763da4bb9a5a5d8acd8962b1bd98
danieltranca <danieltrancax@gmail.com>
2024-10-21 00:36:26 +0200

    chore: wrong flag

--- DotNet/StoreGate/Program.cs
+++ DotNet/StoreGate/Program.cs
@@ -23,7 +23,7 @@ ServiceProvider serviceProvider = serviceCollection.BuildServiceProvider();
 AppDomain.CurrentDomain.UnhandledException += (_, e)
     => serviceProvider.GetRequiredService<ILogger<Program>>().LogCritical((Exception)e.ExceptionObject, "StoreGate encountered an error.");
 
-serviceProvider.GetRequiredService<ILogger<Program>>().LogInformation("hope this works");
+serviceProvider.GetRequiredService<ILogger<Program>>().LogInformation("hope this works1");
 await serviceCollection.BuildServiceProvider().GetRequiredService<CommandRunner>().RunAsync(args);
 
 
