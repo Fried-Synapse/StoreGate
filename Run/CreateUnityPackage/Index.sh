22ea6b2eebf698e21373d37f116a72479f0469bf
danieltranca <danieltrancax@gmail.com>
2024-10-26 23:22:42 +0200

    chore: activating unity

--- Run/CreateUnityPackage/Index.sh
+++ Run/CreateUnityPackage/Index.sh
@@ -3,13 +3,7 @@
 PackagePath="$1"
 PackageName="$2"
 
-echo "ls ."
-ls -l .
-echo "ls UnityProject"
-ls -l UnityProject
-
 ./ActivateUnity.sh
-echo "index from inside"
 
 # unity-editor -batchmode -quit \
 #   -projectPath "${UNITY_PROJECT_PATH}" \
--- Run/sh/ActivateUnity.sh
+++ Run/sh/ActivateUnity.sh
@@ -1,32 +1,30 @@
 #!/bin/sh
 
-echo "ActivateUnity from inside"
+GetSerialFromLicence() 
+{
+    developerData=$(echo "$1" | grep -oP '(?<=<DeveloperData Value=")[^"]*')
 
-# GetSerialFromLicence() 
-# {
-#     developerData=$(echo "$1" | grep -oP '(?<=<DeveloperData Value=")[^"]*')
+    if [ -z "$developerData" ]; then
+        echo "Error: DeveloperData value not found."
+        exit 1
+    fi
 
-#     if [ -z "$developerData" ]; then
-#         echo "Error: DeveloperData value not found."
-#         exit 1
-#     fi
+    # Remove the last 4 characters
+    serial=${developerData:0:${#developerData}-4}
 
-#     # Remove the last 4 characters
-#     serial=${developerData:0:${#developerData}-4}
+    echo "$serial"
+}
 
-#     echo "$serial"
-# }
+UnitySerial="$UNITY_SERIAL"
 
-# UnitySerial="$UNITY_SERIAL"
+# Extract Unity Serial from License if provided
+if [ -n "$UNITY_LICENSE" ]; then
+  UnitySerial=$(GetSerialFromLicence "$UNITY_LICENSE")
+fi
 
-# # Extract Unity Serial from License if provided
-# if [ -n "$UNITY_LICENSE" ]; then
-#   UnitySerial=$(GetSerialFromLicence "$UNITY_LICENSE")
-# fi
-
-# # Activate Unity license using the provided credentials
-# unity-editor -batchmode -quit \
-#   -username "$UNITY_EMAIL" \
-#   -password "$UNITY_PASSWORD" \
-#   -serial "$UnitySerial" \
-#   -logFile /dev/stdout 
+# Activate Unity license using the provided credentials
+unity-editor -batchmode -quit \
+  -username "$UNITY_EMAIL" \
+  -password "$UNITY_PASSWORD" \
+  -serial "$UnitySerial" \
+  -logFile /dev/stdout 
